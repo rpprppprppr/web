d8791dc15f7d086b8641a069558c64afc625ebda
rpprppprppr <kondrenkodo@gmail.com>
2025-04-15 13:13:58 +0500

    finished php

--- src/year.php
+++ src/year.php
@@ -2,11 +2,10 @@
 <html lang="ru">
     <head>
         <meta charset="UTF-8">
-        <title><?= $title ?></title>
+        <title>Задание 5</title>
     </head>
     <body>
         <h4>СПОСОБ 2: код php в отдельном файле</h4>
-        <h1><?= $header ?></h1>
-        <h2>На календаре <?= $currentYear ?> год</h2>
+        <h2>Текущий год <?= $year ?> год</h2>
     </body>
 </html>
