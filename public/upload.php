29a6261fbcaf7851e2ed3899525305f27c2c0668
rpprppprppr <kondrenkodo@gmail.com>
2025-04-29 04:23:18 +0500

    fix

--- public/upload.php
+++ public/upload.php
@@ -8,15 +8,17 @@
     <?php
       $uploaddir = 'images/foto/'; // Relative path under webroot
       $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+      
       //Проверка существует ли файл
       if (file_exists($uploadfile)) { echo "Файл $uploadfile существует, выберите другое имя файла!"; exit;} 
 
       //Проверка на размер файла 
-        if($_FILES["userfile"]["size"] > 1024*1*1024)
-        {
-          echo ("Размер файла не больше 5 мб");
-          exit;
-        }
+      if($_FILES["userfile"]["size"] > 1024*1*1024)
+      {
+        echo ("Размер файла не больше 5 мб");
+        exit;
+      }
+
       //Проверка расширения файла
       $blacklist = array(".php", ".phtml", ".php3", ".php4");
       foreach ($blacklist as $item) {
@@ -24,7 +26,8 @@
         echo "Загрузка php-файлов запрещена!";
         exit;
         }
-        }
+      }
+
       //Проверка на тип файла
       $imageinfo = getimagesize($_FILES['userfile']['tmp_name']);
       if($imageinfo['mime'] != 'image/gif' && $imageinfo['mime'] != 'image/jpeg') {
